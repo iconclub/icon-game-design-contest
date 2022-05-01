a838a95d289f0fdd2798351caa6964ae999516cf
Pham Hong Duc <iamphduc@gmail.com>
2022-05-02 02:36:46 +0700

    fix: too dark modal background

--- src/components/Modal/Modal.svelte
+++ src/components/Modal/Modal.svelte
@@ -47,7 +47,7 @@
     right: 0;
     bottom: 0;
     left: 0;
-    background-color: rgba(0, 0, 0, 0.5);
+    background-color: rgba(0, 0, 0, 0.1);
     width: 100%;
     height: 100%;
     min-height: 70vh;
